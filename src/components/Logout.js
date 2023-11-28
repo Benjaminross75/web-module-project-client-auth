4c8be6e267cfb6ca4725a70871728a65555e4021
Ben Ross <mrbenross@gmail.com>
2023-11-28 12:23:17 -0600

    Logout component updated

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () =>{
-    return(<h2>Logout</h2>)
+    const navigate = useNavigate()
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+        axios.post("http://localhost:9000/api/logout",{},{
+            headers:{
+                authorization: token
+            }
+        })
+        .then(res =>{
+            localStorage.removeItem('token')
+            navigate('/login')
+        })
+        .catch(err => console.error(err))
+    },[])
+    return(<div>
+        <h2>Logout</h2>
+    </div>)
   }
 export default Logout
