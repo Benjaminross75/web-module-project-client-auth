8870acd56c0704d096b58f7cce89ecd88ea9cf83
Ben Ross <mrbenross@gmail.com>
2023-11-28 10:24:34 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+const Login = () =>{
+  return(<div>
+    <h1>Login</h1>
+    <label
+  </div>)
+}
+
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/friends' element={<FriendsList/>}/>
+      <Route path='/friends/add' element={<AddFriend/>}/>
+      <Route path='/logout' element={<Logout/>}/>
+    </Routes>
     </div>
   );
 }
