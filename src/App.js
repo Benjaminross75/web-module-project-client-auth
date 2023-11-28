5fc8e56bcf51376aa3db857852db0a617f4f7b11
Ben Ross <mrbenross@gmail.com>
2023-11-28 10:43:44 -0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import AddFriend from './components/AddFriend';
+import Logout from './components/Logout';
 
-const Login = () =>{
-  return(<div>
-    <h1>Login</h1>
-    <label
-  </div>)
-}
+
+
 
 
 function App() {
---
+++ src/components/AddFriend.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AddFriend = () =>{
+    return(<h2>Add a friend</h2>)
+    }
+
+
+export default AddFriend
